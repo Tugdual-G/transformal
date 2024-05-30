a78e5420a406d4b8203fabf5850834465ae71974
Tugdual <tugdualgaillard@gmail.com>
2024-05-30 14:36:24 +0200

    basic implementation

--- transform.py
+++ transform.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
-# from matplotlib.pyplot import axis
-from matplotlib.pyplot import tight_layout
 import numpy as np
 import trimesh
 import networkx as nx
 from scipy.sparse.linalg import spsolve, norm, eigsh, inv
-from scipy.sparse import csc_array
+from scipy.sparse import csc_array, csr_array
 from scipy.linalg import issymmetric, solve, expm_cond, eigh
 from trimesh_curvature import scalar_curvature, mean_curvature
+from numba import jit
+
+
+def mul_quatern(u, v):
+    w0, x0, y0, z0 = u
+    w1, x1, y1, z1 = v
+    return np.array([-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
+                    x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
+                    -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
+                    x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=np.float64)
 
 def create_dirac_op_vv(trimesh, rho):
     vertices = trimesh.vertices
@@ -54,13 +62,6 @@ def create_dirac_op_vv(trimesh, rho):
     return X_block
 
 
-def mul_quatern(u, v):
-    w0, x0, y0, z0 = u
-    w1, x1, y1, z1 = v
-    return np.array([-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
-                    x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
-                    -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
-                    x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=np.float64)
 
 
 def new_edges_divergence(trimesh, lambd):
@@ -95,7 +96,6 @@ def new_edges_divergence(trimesh, lambd):
             # one_ordered[i][0] = one_ordered[i][0][::sign]
 
             ring_nv = ring_vert.shape[0]
-
             for k in range(ring_nv):
                 e1 = -edges_vect[(k-1)%ring_nv]
                 o1 =  edges_vect[k]+e1
@@ -134,11 +134,17 @@ def quaternionic_laplacian_matrix(trimesh):
     g = nx.from_edgelist(trimesh.edges_unique)
     one_ring = [list(g[i].keys()) for i in range(nv)]
     one_ordered = [nx.cycle_basis(g.subgraph(i)) for i in one_ring]
-
     L = np.zeros((nv*4,nv*4))
 
+    idx_i = np.zeros(nv*4*10, dtype=np.int_)
+    idx_j = np.zeros(nv*4*10, dtype=np.int_)
+    data = np.zeros(nv*4*10,dtype=np.float64)
+
+    cot = 0.0
+    diag = 0.0
+    sp_k = 0
     for i in range(nv):
-        if len(one_ordered[i]) >0:
+        if len(one_ordered[i][0]) >0:
             ring_vert = trimesh.vertices[one_ordered[i][0]]
 
             # edges connecting the point to itś neighbours
@@ -152,12 +158,13 @@ def quaternionic_laplacian_matrix(trimesh):
 
             ring_nv = ring_vert.shape[0]
             # iterating over each of the edges adjacent to the vertex i
+            diag = 0.0
             for k in range(ring_nv):
                 e1 = -edges_vect[(k-1)%ring_nv]
                 o1 =  edges_vect[k]+e1
                 cos1 = np.dot(e1,o1)
                 sin1 = np.linalg.norm(np.cross(o1,e1))
-                cot1 = cos1/sin1
+                cot = cos1/sin1
 
                 e2 = -edges_vect[(k+1)%ring_nv]
                 o2 =  edges_vect[k]+e2
@@ -165,21 +172,30 @@ def quaternionic_laplacian_matrix(trimesh):
                 cos2 = np.dot(e2,o2)
                 sin2 = np.linalg.norm(np.cross(e2,o2))
 
-                cot2 = cos2/sin2
+                cot += cos2/sin2
 
                 j = one_ordered[i][0][k]
+                diag -= cot
                 for l in range(4):
-                    L[4*i+l,4*j+l] = (cot2+cot1)#/(2*a_x2)
-                    L[4*i+l,4*i+l] -= (cot2+cot1)#/(2*a_x2)
+                    idx_i[sp_k] = i * 4 + l
+                    idx_j[sp_k] = j * 4 + l
+                    data[sp_k] = cot
+                    sp_k += 1
 
-    return L
+            for l in range(4):
+                idx_i[sp_k] = i * 4 + l
+                idx_j[sp_k] = i * 4 + l
+                data[sp_k] = diag
+                sp_k += 1
+
+    return idx_i[:sp_k], idx_j[:sp_k], data[:sp_k]
 
 def eigensolve(M, v):
     nv = v.shape[0]
     v[:] = 0
     v[::4] = 1/nv
     M = M
-    for i in range(15):
+    for i in range(4):
         v[:] = spsolve(M, v)
         v.shape = (nv//4,4)
         v /= np.sqrt(np.sum(np.sum(v**2,axis=1)))
@@ -191,14 +207,33 @@ def eigensolve(M, v):
     v.shape = (nv, )
 
 
+def symetric_delete(i_del, idx_i, idx_j, data, n):
+    idx_table = np.empty(n, np.int_)
+    idx_sp = 0
+    for k in range(n):
+        if k not in i_del:
+            idx_table[k] = idx_sp
+            idx_sp +=1
+
+    idx_sp = 0
+    for k in range(data.shape[0]):
+        if (idx_i[k] not in i_del) and (idx_j[k] not in i_del):
+            idx_i[idx_sp] = idx_table[idx_i[k]]
+            idx_j[idx_sp] = idx_table[idx_j[k]]
+            data[idx_sp] = data[k]
+            idx_sp += 1
+
+    idx_i = idx_i[:idx_sp]
+    idx_j = idx_j[:idx_sp]
+    data = data[:idx_sp]
+    return idx_i, idx_j, data
+
+# def symetric_delete2(i_del, L):
+#     L_wp = np.delete(L,i_del, axis=0)
+#     return np.delete(L_wp, i_del, axis=1)
+
 
 def transform(trimesh, rho):
-
-    L = quaternionic_laplacian_matrix(trimesh)
-    L = (L+L.T)/2
-
-    print(f"{issymmetric(L) = }")
-
 
     vertices = trimesh.vertices
     nv = vertices.shape[0]
@@ -213,33 +248,38 @@ def transform(trimesh, rho):
 
     div_e, constraint = new_edges_divergence(trimesh, lambd)
 
+    idx_i, idx_j, data = quaternionic_laplacian_matrix(trimesh)
+
+    # csc for row slicing
+    L = csc_array((data, (idx_i, idx_j)),shape=(nv*4,nv*4))
 
     # Applying constraint to the system
     # TODO make it work for bounded shapes
     for c in constraint:
         i = c[0]
         div_e[:] -= L[:,i*4:i*4+4] @ c[1].T
+
     i_sorted = np.argsort([constraint[0][0],constraint[1][0]])
     i1 = constraint[i_sorted[0]][0]
     i2 = constraint[i_sorted[1]][0]
-    i2_del = [i2*4+i for i in range(4)]
-    i1_del = [i1*4+i for i in range(4)]
-    # Order matters
-    div_wp = np.delete(div_e, i2_del, axis=0)
-    div_wp = np.delete(div_wp, i1_del, axis=0)
-    L_wp = np.delete(L,i2_del, axis=0)
-    L_wp = np.delete(L_wp, i2_del, axis=1)
-    L_wp = np.delete(L_wp,i1_del, axis=0)
-    L_wp = np.delete(L_wp, i1_del, axis=1)
 
-    print(f"{issymmetric(L) = }")
-    L = csc_array(L_wp)
+    i_del = [ic*4+i for ic in [i1, i2] for i in range(4)]
 
-    norm_L = norm(L)
-    inv_L = inv(L)
-    norm_invA = norm(inv_L)
-    cond = norm_L*norm_invA
-    print(f"L condition number = {cond}")
+    idx_i, idx_j, data = symetric_delete(i_del, idx_i, idx_j, data, nv*4)
+
+    # csr
+    L = csc_array((data, (idx_i, idx_j)),shape=((nv-2)*4,(nv-2)*4))
+    # Ls = (L + L.T)/2
+    # print(f"{issymmetric(L.todense()) = }")
+
+    div_wp = np.delete(div_e, i_del, axis=0)
+
+
+    # norm_L = norm(L)
+    # inv_L = inv(L)
+    # norm_invA = norm(inv_L)
+    # cond = norm_L*norm_invA
+    # print(f"L condition number = {cond}")
 
     new_vertices_wp = spsolve(L, div_wp)
     # new_vertices = solve(L, div_e, assume_a='sym')
@@ -276,15 +316,25 @@ if __name__=="__main__":
     print("mean curvature =", mk)
 
     vertices = trimesh.vertices
-    # nv = vertices.shape[0]
-    # triangles = vertices[trimesh.faces]
-    # face_center = np.mean(triangles, axis=1)
+    nv = vertices.shape[0]
 
-    dist1 = np.linalg.norm(vertices-vertices[-1],axis=1)
-    dist2 = np.linalg.norm(vertices-vertices[50],axis=1)
 
-    rho = 10*np.exp(-dist1**2/200)*abs(mk)
-    rho += 10*np.exp(-dist2**2/100)*abs(mk)
+    R = 50
+    pts = []
+    pts += [[np.pi/4, 2*np.pi/6]]
+    pts += [[-3*np.pi/4, 0]]
+    pts += [[-np.pi/4, -np.pi/6]]
+    ampl = np.array([-50, -20, -15])*abs(mk)
+    rad = [50.0, 200.0, 100.0]
+
+    rho = np.zeros(nv,dtype=np.float64)
+    for i, pt in enumerate(pts):
+        pt_cart = np.array([R*np.cos(pt[1])*np.cos(pt[0]),
+                    R*np.cos(pt[1])*np.sin(pt[0]),
+                    R*np.sin(pt[1])])
+        dist = np.linalg.norm(vertices-pt_cart,axis=1)
+        rho += ampl[i]*np.exp(-dist**2/rad[i])
+
 
 
     rho_fc = vertex2face(trimesh, rho)
@@ -307,6 +357,14 @@ if __name__=="__main__":
     zlim = [(vertices[:,2].min(), vertices[:,2].max())]
 
     transform(trimesh, rho)
+
+    kN = mean_curvature(trimesh)
+    k = scalar_curvature(trimesh, kN)
+
+    k_fc = vertex2face(trimesh, k)
+    k_norm = (k_fc-k_fc.min())/np.ptp(k_fc)
+    cm = plt.cm.plasma(k_norm)
+
     triangles = vertices[trimesh.faces]
     pc = art3d.Poly3DCollection(triangles, facecolors=cm,
                                 shade=True, edgecolors=(1,1,1,0.2),
@@ -331,4 +389,5 @@ if __name__=="__main__":
     # trimesh.visual.face_colors = cm
     # trimesh.export("sphananaHQ.ply")
     fig.tight_layout(pad=0)
+    # fig.savefig("ballfig.png")
     plt.show()
