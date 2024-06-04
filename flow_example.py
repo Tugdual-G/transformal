6fc66b5db0ec243e235d193dbd7500bfaa6ed272
Tugdual <tugdualgaillard@gmail.com>
2024-06-04 16:36:50 +0200

    cleaning

--- flow_example.py
+++ flow_example.py
@@ -23,10 +23,10 @@ def vertex2face(trimesh, vval):
 
 
 def to_cmap(v, v_min=None, v_max=None):
-    if not v_min:
+    if v_min is None:
         v_min = v.min()
 
-    if not v_max:
+    if v_max is None:
         v_max = v.max()
     return plt.cm.plasma((v - v_min) / (v_max - v_min))
 
@@ -36,47 +36,48 @@ vertices = trimesh.vertices
 nv = vertices.shape[0]
 nf = trimesh.faces.shape[0]
 print("number of vertices", nv)
-print(f"{trimesh.vertex_normals.shape}")
 
+# center the object
 centerofmass = np.mean(vertices, axis=0)
 vertices[:] = vertices - centerofmass
-vol0 = trimesh.volume
+# vol0 = trimesh.volume
+
+# The one ring is needed for many subsequent routines
 one_ring = get_oriented_one_ring(trimesh)
 
-k = scalar_curvature(trimesh, mean_curvature(vertices, one_ring))
-k_min = k.min()
-k_max = k.max()
-
+# Defines the plot
 fig, ax = plt.subplots(1, 1, figsize=(8, 7), subplot_kw=dict(projection="3d"))
 ax.axis("off")
 
-
-fraction = 0.8
+# Use cuvature to define the coloration of the mesh's faces
+k = scalar_curvature(trimesh, mean_curvature(vertices, one_ring))
+k_min = k.min()
+k_max = k.max()
+# Define the colors of the mesh
 cm = to_cmap(vertex2face(trimesh, k), k_min, k_max)
+
+# Defines illumination
 light = LightSource(90, 100)
+fraction = 0.9
 intensity = light.shade_normals(trimesh.face_normals, fraction=fraction)
 intensity.shape = (nf, 1, 1)
 cm.shape = (nf, 1, 4)
 cm = light.blend_overlay(cm, intensity=intensity)
 cm.shape = (nf, 4)
 
-fig.tight_layout(pad=0)
+# creates the poligon collection (mesh faces)
 triangles = vertices[trimesh.faces]
-pc = art3d.Poly3DCollection(
+polycol = art3d.Poly3DCollection(
     triangles,
     facecolors=cm,
     edgecolors=(1, 1, 1, 0.2),
     alpha=1,
-    lightsource=light,
 )
 
 
-def init_func(ax, pc, trimesh, fig):
-
-    ax.add_collection(pc)
-    xlim = (vertices[:, 0].min(), vertices[:, 0].max())
-    ylim = (vertices[:, 1].min(), vertices[:, 1].max())
-    zlim = (vertices[:, 2].min(), vertices[:, 2].max())
+def init_func(ax, polycol, vertices, fig):
+    """Intialize the plot for animation."""
+    ax.add_collection(polycol)
     lim = (vertices.min(), vertices.max())
     ax.set_xlim(*lim)
     ax.set_ylim(*lim)
@@ -88,25 +89,20 @@ def init_func(ax, pc, trimesh, fig):
     fig.tight_layout(pad=0)
 
 
-dt = 0.4
-
-polycol = pc
-
-
 def animate(i):
+    """Animation funcion"""
+    dt = 0.5
     if i > 5:
         k = scalar_curvature(trimesh, mean_curvature(vertices, one_ring))
         flow(vertices, -dt * k, one_ring)
-        dist = (vol0 / trimesh.volume) ** (1 / 3)
         centerofmass = np.mean(vertices, axis=0)
-        vertices[:] = dist * (vertices - centerofmass)
+        vertices[:] = vertices - centerofmass
         cm = to_cmap(vertex2face(trimesh, k), k_min, k_max)
         intensity = light.shade_normals(trimesh.face_normals, fraction=fraction)
         intensity.shape = (nf, 1, 1)
         cm.shape = (nf, 1, 4)
         cm = light.blend_overlay(cm, intensity=intensity)
         cm.shape = (nf, 4)
-        # cm[:] *= cm * intensity[:, np.newaxis]
         polycol.set_facecolor(cm)
         polycol.set_verts(vertices[trimesh.faces])
         return (polycol,)
@@ -116,10 +112,10 @@ ani = animation.FuncAnimation(
     fig,
     animate,
     frames=60,
-    interval=60,
+    interval=50,
     blit=False,
     repeat=False,
-    init_func=partial(init_func, ax, pc, trimesh, fig),
+    init_func=partial(init_func, ax, polycol, vertices, fig),
 )
 plt.show()
 # ani.save("animation.gif")
